a76df817b5a7506d222913f97c636b85ba0b71f4
Idiljei <i.jeilani@gmail.com>
2021-01-26 23:42:50 +0000

    client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,17 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// $(document).ready(function () {
+//   //function gets loaded when everything is loaded on page
+
+//   $("#tweet-text").on("keyup", function () {
+//     onkeyUp($(this)); //calls the function for this eventhandler
+//   });
+// });
+
+// // make functions outside the doc.ready and then call them inside doc.ready on the particular selector using $this as param
+// const onkeyUp = (textarea) => {
+//   console.log(textarea.val()); // will print all the words in my textbox
+// };
+
+
