871eca528cccb8905a67de251007237e0dde1526
Idiljei <i.jeilani@gmail.com>
2021-01-27 04:17:02 +0000

    composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#tweet-text").on("input", function (event) {
+    const charsLeft = 140 - $(this).val().length; //val is getting the text from text area - not setting new value
 
-  $('#tweet-text').on('input', function(event) {
-  const charsLeft = (140 - $(this).val().length); //val is getting the text from text area - not setting new value 
-  
-  $(this).siblings().children(".counter").text(charsLeft);
+    let counter = $(this).siblings().children(".counter").text(charsLeft);
 
-  })
-
+    if (charsLeft < 0) {
+      counter.addClass("addColour");
+    } else {
+      counter.removeClass("addColour");
+    }
+  });
 });
-
-
