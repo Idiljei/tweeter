a564a2a29dd442d52ffab05d8e64fec806487f41
Idiljei <i.jeilani@gmail.com>
2021-01-28 00:20:26 +0000

    client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,85 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// $(document).ready(function () {
-//   //function gets loaded when everything is loaded on page
+const tweetData = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text:
+        "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1611592370145,
+  },
 
-//   $("#tweet-text").on("keyup", function () {
-//     onkeyUp($(this)); //calls the function for this eventhandler
-//   });
-// });
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1611678770146,
+  },
+];
 
-// // make functions outside the doc.ready and then call them inside doc.ready on the particular selector using $this as param
-// const onkeyUp = (textarea) => {
-//   console.log(textarea.val()); // will print all the words in my textbox
-// };
+//  takes in tweet object and returns tweet <article>
+createTweetElement = function (tweetData) {
+  // Make variable that targets html property using jquery
+  //target article in index --> once you have target and append the html
+  // return
 
+  const tweetElement = `
+<div class="tweet-container">
+<!-- first div -->
+<div class="tweet-container-userinfo">
+  <div class="tweet-profile">
+    <img src=${tweetData.user.avatars} alt="Profile Image" />
+    <p> ${tweetData.user.name}</p>
+  </div>
+  <div class="tweet-username">${tweetData.user.handle}</div>
+</div>
+<!-- body div -->
+<div class="tweet-body">${tweetData.content.text}css</div>
+<!-- tweet action div  -->
+<div class="tweet-actions">
+  <div class="tweet-timestamp">${tweetData.created_at}</div>
+  <div class="tweet-icons">
+    <i class="fas fa-flag"></i>
+    <i class="far fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heart"></i>
+    <i class="far fa-heart"></i>
+  </div>
+</div>
+`;
+  return tweetElement;
+};
 
+renderTweets = function (tweets) {
+  for (let tweet of tweets) {
+    $(".tweets-containers").append(createTweetElement(tweet));
+  }
+};
+
+$(document).ready(function () {
+  renderTweets(tweetData);
+
+  // For Form
+  $("#formtweet").submit(function (event) {
+    event.preventDefault();
+    let seralizedData = $("#tweet-text").serialize();
+    
+    $.ajax( {
+      method: "POST",
+      url: "/tweets",
+      data: seralizedData,
+     }).then(console.log("itworked!"));
+
+  });
+});
